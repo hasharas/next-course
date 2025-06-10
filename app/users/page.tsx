52b8139857975e825628038aa4f7414b11fea14e
hasharas <hasharamsankalpam@gmail.com>
2025-06-11 01:23:53 +0530

    fetch:practice

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
-const UserPage = () => {
+const UserPage = async () => {
+
+  interface Users {
+    id:number;
+    name:string;
+    website:string;
+  }
+
+  const fetchdata = await fetch('https://jsonplaceholder.typicode.com/users');
+  const datas: Users [] = await fetchdata.json(); 
+  console.log("users", datas)
+
+
   return (
     <div className='p-4 bg-green-500'>
       <h1 className='font-green text-xs text-green-800'>user page 1</h1>
-            </div>
+
+      <ul>
+        {datas.map( data  => (
+          <li key={data.id}>
+            <p>{data.name}</p>
+            <span>{data.website}</span>
+          </li>
+        )
+
+        )
+
+        }
+      </ul>
+    </div>
   );
 }
 
